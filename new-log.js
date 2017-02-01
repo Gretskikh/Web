3bb85df89db0b1fd5012352f080c574b191efde4
Anatoly Gretskikh <gretskikh@gmail.com>
2017-02-01 14:24:04 +0500

    Added counter function

---
+++ new-log.js
@@ -0,0 +1,13 @@
+var counter = (function(){
+	var count = 0;
+	return function(num){
+		count = num !== undefined ? num : count;
+		return count++;
+	}
+}());
+
+console.log(counter());
+console.log(counter());
+console.log(counter(50));
+console.log(counter());
+console.log(counter());
